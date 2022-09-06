0efb750279ba65d3f7c320dee68efc804c2fbb79
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:10:40 -0700

    Add files via upload

---
+++ the_overview_of_IT_industry.c
@@ -0,0 +1,27 @@
+           "OVERVIEW OF IT INDUSTRY"
+    
+* what is meaning by IT industry?
+=>IT sector include companies that produce 
+   software, hardware  or semiconductorequipment,
+   and companies that proived internet or
+   related services.
+   
+=>INFORMATION TECHNOLOGY (IT) is the use of
+ computers and softwareto manage information.
+   
+=>it is the career disciplin responsible for 
+ storing informathion, precting informathion , 
+ proccesing the information ,transmitting the 
+ information as nessessary , and later retrieving
+ information as necessary.
+ 
+ => The Information Technology industry is one 
+   of the fastest-growing sectors worldwide 
+   providing jobs full of opportunities for
+   professional success. 
+   
+=>	The IT industry offers quick employment,
+   opportunities in every sector, multiple 
+	career paths,  high salaries, and it's
+   easy to enter this industry without a college degree.
+	  
