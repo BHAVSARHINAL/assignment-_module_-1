9f734ff72349dfebacb70c746d884d0f65e0ad38
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:46:17 -0700

    Add files via upload

---
+++ conditionuseinalphabets.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+
+   //   a,e,i,o,u  ==>vowel
+   // remaining all==>consonant
+    
+    char ch;
+     printf("enter any alphabet ");
+	 scanf("%c",&ch);
+	    
+		if(ch== 'a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u')
+		   {
+		   	
+		   	
+		printf("charecter is a vowel");
+	}
+		else
+			{
+			printf(" charecter is a consonant");
+			
+			
+			 } 
+		
+		 
+    
+    
+    
+    
+    
+    
+    
+    
+}
