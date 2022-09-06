10367675ec72c18101b6127d9939cc922e90cfe6
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:44:43 -0700

    Add files via upload

---
+++ capitalsmalllateer.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+	
+	
+	
+	
+ 	char ch;
+	printf("enter any character");
+	scanf("%c",&ch);
+ 
+ 	
+	
+if ( (ch>='A') && (ch<='Z'))
+{
+
+		printf("\n this is a capital later");
+}
+ else if ( (ch>='a') && (ch<='z'))
+{
+
+	 	printf("\n this is a small later");
+}
+else if ( (ch>='0') && (ch<='9'))
+{
+
+		printf("\n this is a digit");
+}
+   	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
