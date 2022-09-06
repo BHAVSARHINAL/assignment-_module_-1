b6f3658e00ce9956bf0db6ac8f37cffd6bf8ba4b
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:48:41 -0700

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	
+	
+	int num;
+	 scanf("%d",&num);
+	 
+	 if(num>=0)
+	 { 
+	 
+	 printf("num is positive");
+	 }
+	 
+	 else
+
+    printf("num is nagative");
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+ return 0;
+
+
+
+
+
+}
