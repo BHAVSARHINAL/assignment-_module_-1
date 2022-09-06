1074e24eba7864bf52084830439792bfef1b0b4c
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:47:46 -0700

    Add files via upload

---
+++ elseifelse.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	
+	scanf("%d",&num);
+	
+	if(num>0)
+	{
+		printf("print a posi num");
+	}
+	
+	else if(num==0)
+    
+	{
+		printf( "print a equal num");
+		
+		}	
+	else
+	
+	{
+		printf("print a nag num");
+	}
+	
+	
+	return 0;
+	
+	
+	
+	
+}
