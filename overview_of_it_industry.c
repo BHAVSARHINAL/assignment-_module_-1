640609ac8215c2458ec5dfbc92d95adb5ca6ee9e
BHAVSARHINAL <110092239+BHAVSARHINAL@users.noreply.github.com>
2022-09-06 10:07:11 -0700

    Add files via upload

---
+++ overview_of_it_industry.c
@@ -0,0 +1,17 @@
+           OVERVIEW OF IT INDUSTRY
+    
+* what is meaning by IT industry?
+=>IT sector include companies that produce 
+   software, hardware  or semiconductorequipment,
+   and companies that proived internet or
+   related services.
+   
+=>INFORMATION TECHNOLOGY (IT) is the use of
+ computers and softwareto manage information.
+   
+=>it is the career disciplin responsible for 
+ storing informathion, precting informathion , 
+ proccesing the information ,transmitting the 
+ information as nessessary , and later retrieving
+ information as necessary.
+    
